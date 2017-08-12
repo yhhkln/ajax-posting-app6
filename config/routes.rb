6991bd86bb0d8458bf6b87cfdea78cf0318546b0
杨昊霖 <yanghaolin@yanghaolindeMacBook-Pro.local>
2017-08-12 11:53:23 +0800

    jquery1

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-	get "/jquery-1" => "pages#query_1"
-	get "/jquery-2" => "pages#query_2"
-	get "/jquery-3" => "pages#query_3"
-	get "/jquery-4" => "pages#query_4"
-	get "/jquery-5" => "pages#query_5"
+	get "/jquery-1" => "pages#jquery_1"
+	get "/jquery-2" => "pages#jquery_2"
+	get "/jquery-3" => "pages#jquery_3"
+	get "/jquery-4" => "pages#jquery_4"
+	get "/jquery-5" => "pages#jquery_5"
+	root "pages#jquery_1"
 end
