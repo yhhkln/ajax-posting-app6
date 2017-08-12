9d1069a02f6cc41e2062123aba0ff86e5fe3d215
杨昊霖 <yanghaolin@yanghaolindeMacBook-Pro.local>
2017-08-12 11:43:28 +0800

    cvr

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	get "/jquery-1" => "pages#query_1"
+	get "/jquery-2" => "pages#query_2"
+	get "/jquery-3" => "pages#query_3"
+	get "/jquery-4" => "pages#query_4"
+	get "/jquery-5" => "pages#query_5"
 end
