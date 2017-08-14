d6abea8d720ab8ba2f152e39374d486ee7192dc4
杨昊霖 <yanghaolin@yanghaolindeMacBook-Pro.local>
2017-08-14 19:34:10 +0800

    json delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,6 +13,7 @@ class PostsController < ApplicationController
 	def destroy
 		@post = current_user.posts.find(params[:id])
 		@post.destroy
+    render :json => { :id => @post.id }
 	end
 
   def like
